1aa75ee710cbbb8f1644fc55de3be3045266389b
liaa114 <emeliafresianandela@gmail.com>
2026-05-09 21:44:51 +0700

    Add sweetalert jika search empty

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react"
+import Swal from "sweetalert2"
+
 export default function EmptyState({
   error,
   isLoading,
@@ -5,26 +8,92 @@ export default function EmptyState({
   showFavorites,
   debouncedSearch,
 }) {
-  return (
-    <div className="mt-6">
 
-      {error && (
-        <p className="text-sm text-red-400">
-          Something went wrong
-        </p>
-      )}
+  useEffect(() => {
 
-      {!isLoading &&
-        movies.length === 0 &&
-        (showFavorites ||
-          debouncedSearch.trim() !== "") && (
-          <p className="text-sm text-zinc-300">
-            {showFavorites
-              ? "No favorite movies yet"
-              : "Movie not found"}
-          </p>
-      )}
+    if (error) {
 
-    </div>
-  )
+      let title = "Something went wrong"
+      let text =
+        "Failed to fetch movie data"
+
+      if (
+        error?.response?.status === 429
+      ) {
+        title = "API Limit Reached"
+
+        text =
+          "Too many requests. Please wait a moment and try again."
+      }
+
+      else if (
+        error?.message?.includes(
+          "Network"
+        )
+      ) {
+        title = "Connection Error"
+
+        text =
+          "Please check your internet connection."
+      }
+
+      Swal.fire({
+        icon: "error",
+        title,
+        text,
+        background: "#18181b",
+        color: "#fff",
+        confirmButtonColor:
+          "#ec4899",
+        backdrop: `
+          rgba(0,0,0,0.8)
+        `,
+        customClass: {
+          popup:
+            "rounded-3xl border border-white/10",
+        },
+      })
+    }
+
+  }, [error])
+
+  useEffect(() => {
+
+    if (
+      !isLoading &&
+      movies.length === 0 &&
+      (showFavorites ||
+        debouncedSearch.trim() !== "")
+    ) {
+
+      Swal.fire({
+        icon: "info",
+        title: showFavorites
+          ? "No Favorites Yet"
+          : "Movie Not Found",
+        text: showFavorites
+          ? "You haven't added favorite movies yet."
+          : "Try searching with another keyword.",
+        background: "#18181b",
+        color: "#fff",
+        confirmButtonColor:
+          "#ec4899",
+        backdrop: `
+          rgba(0,0,0,0.8)
+        `,
+        customClass: {
+          popup:
+            "rounded-3xl border border-white/10",
+        },
+      })
+    }
+
+  }, [
+    isLoading,
+    movies,
+    showFavorites,
+    debouncedSearch,
+  ])
+
+  return null
 }
